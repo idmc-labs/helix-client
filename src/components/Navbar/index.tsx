1b10118428014fb16e868319337155927d9e435d
tnagorra <weathermist@gmail.com>
2020-08-17 16:09:59 +0545

    Add Sign Out button on Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext, useCallback } from 'react';
 import { NavLink } from 'react-router-dom';
+import { gql, useMutation } from '@apollo/client';
 import { _cs } from '@togglecorp/fujs';
 
+import { Button } from '@togglecorp/toggle-ui';
+
+import DomainContext from '#components/DomainContext';
+
 import styles from './styles.css';
+
+const LOGOUT = gql`
+  mutation Logout {
+    logout {
+        ok
+    }
+  }
+`;
 
 interface Props {
     className?: string;
@@ -11,12 +24,41 @@ interface Props {
 const Navbar = (props: Props) => {
     const { className } = props;
 
+    const {
+        authenticated,
+        setUser,
+    } = useContext(DomainContext);
+
+    interface LogoutResponse {
+        logout: {
+            ok: boolean;
+        }
+    }
+
+    const [logout] = useMutation<LogoutResponse>(
+        LOGOUT,
+        {
+            onCompleted: (data: LogoutResponse) => {
+                if (data.logout.ok) {
+                    setUser(undefined);
+                }
+            },
+        },
+    );
+
+    const handleLogout = useCallback(
+        () => {
+            logout();
+        },
+        [],
+    );
+
     return (
         <nav className={_cs(className, styles.navbar)}>
             <div className={styles.appBrand}>
                 Helix
             </div>
-            <div className={styles.actions}>
+            <div className={styles.main}>
                 <div className={styles.navLinks}>
                     <NavLink
                         exact
@@ -27,6 +69,15 @@ const Navbar = (props: Props) => {
                         Home
                     </NavLink>
                 </div>
+            </div>
+            <div className={styles.actions}>
+                {authenticated && (
+                    <Button
+                        onClick={handleLogout}
+                    >
+                        Sign Out
+                    </Button>
+                )}
             </div>
         </nav>
     );
