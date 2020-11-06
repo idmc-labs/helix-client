4979db55a5339bcd4dfff60152bc79f176273337
tnagorra <weathermist@gmail.com>
2020-11-06 12:17:29 +0545

    Add file-upload and retry capability on request

--- src/Root/App/index.tsx
+++ src/Root/App/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloLink } from 'apollo-link';
+import { RetryLink } from 'apollo-link-retry';
+// import { BatchHttpLink } from 'apollo-link-batch-http';
+import { createUploadLink } from 'apollo-upload-client';
 
 import '@togglecorp/toggle-ui/build/index.css';
 import './styles.css';
@@ -8,14 +12,27 @@ import Multiplexer from './Multiplexer';
 
 const GRAPHQL_ENDPOINT = process.env.REACT_APP_GRAPHQL_ENDPOINT as string;
 
-// FIXME: need to change credentials this to same-site on production
-const link = createHttpLink({
-    uri: GRAPHQL_ENDPOINT,
-    credentials: 'include',
-});
-
 const client = new ApolloClient({
-    link,
+    link: ApolloLink.from([
+        new RetryLink(),
+        ApolloLink.split(
+            (operation) => operation.getContext().hasUpload,
+            createUploadLink({
+                uri: GRAPHQL_ENDPOINT,
+                credentials: 'include',
+            }),
+            createHttpLink({
+                uri: GRAPHQL_ENDPOINT,
+                credentials: 'include',
+            }),
+            /*
+            new BatchHttpLink({
+                uri: GRAPHQL_ENDPOINT,
+                credentials: 'include',
+            }),
+            */
+        ),
+    ]),
     cache: new InMemoryCache(),
     defaultOptions: {
         watchQuery: {
