33b3df74113ec275c46239788effc55915d3ec88
ShrawanBK <bkshrawanbk@gmail.com>
2020-11-18 15:35:31 +0545

    List, Add, Edit organization

---
+++ src/components/Organization/OrganizationForm/index.tsx
@@ -0,0 +1,361 @@
+import React from 'react';
+
+import {
+    TextInput,
+    SelectInput,
+    Button,
+    TextArea,
+} from '@togglecorp/toggle-ui';
+
+import {
+    gql,
+    useMutation,
+    useQuery,
+    MutationUpdaterFn,
+} from '@apollo/client';
+
+import useForm, { createSubmitHandler } from '#utils/form';
+import type { Schema } from '#utils/schema';
+import { removeNull } from '#utils/schema';
+import { transformToFormError } from '#utils/errorTransform';
+
+import {
+    BasicEntity,
+    PartialForm,
+    PurgeNull,
+} from '#types';
+
+import {
+    basicEntityKeySelector,
+    basicEntityLabelSelector,
+} from '#utils/common';
+
+import {
+    idCondition,
+    requiredStringCondition,
+    lengthSmallerThanCondition,
+} from '#utils/validation';
+
+import {
+    OrganizationKindListQuery,
+    OrganizationQuery,
+    CreateOrganizationMutation,
+    CreateOrganizationMutationVariables,
+    UpdateOrganizationMutation,
+    UpdateOrganizationMutationVariables,
+} from '#generated/types';
+
+import styles from './styles.css';
+
+const GET_ORGANIZATION_KIND_LIST = gql`
+    query OrganizationKindList {
+        organizationKindList {
+            results {
+                id
+                name
+            }
+        }
+    }
+`;
+
+const CREATE_ORGANIZATION_KIND = gql`
+    mutation CreateOrganization($organization: OrganizationCreateInputType!) {
+        createOrganization(data: $organization) {
+            result {
+                id
+                name
+                shortName
+                organizationKind {
+                    id
+                    name
+                  createdAt
+                }
+                methodology
+                breakdown
+            }
+            errors {
+                field
+                messages
+            }
+        }
+    }
+`;
+
+const UPDATE_ORGANIZATION_KIND = gql`
+    mutation UpdateOrganization($organization: OrganizationUpdateInputType!) {
+        updateOrganization(data: $organization) {
+            result {
+                id
+                name
+                shortName
+                organizationKind {
+                    id
+                    name
+                  createdAt
+                }
+                methodology
+                breakdown
+            }
+            errors {
+                field
+                messages
+            }
+        }
+    }
+`;
+
+const ORGANIZATION = gql`
+    query Organization($id: ID!) {
+        organization(id: $id) {
+            id
+            name
+            shortName
+            organizationKind {
+                id
+                name
+            }
+            methodology
+            breakdown
+        }
+    }
+`;
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type WithId<T extends object> = T & { id: string };
+type OrganizationFormFields = CreateOrganizationMutationVariables['organization'];
+type FormType = PurgeNull<PartialForm<WithId<Omit<OrganizationFormFields, 'designation' | 'gender'> & {designation: BasicEntity['id'], gender: BasicEntity['id']}>>>;
+
+const schema: Schema<FormType> = {
+    fields: () => ({
+        id: [idCondition],
+        organizationKind: [],
+        shortName: [requiredStringCondition, lengthSmallerThanCondition(6)],
+        name: [requiredStringCondition],
+        methodology: [requiredStringCondition],
+        breakdown: [],
+    }),
+};
+
+const defaultFormValues: PartialForm<FormType> = {};
+
+interface OrganizationFormProps {
+    id?: string | undefined;
+    onHideAddOrganizationModal: () => void;
+    onAddOrganizationCache: MutationUpdaterFn<CreateOrganizationMutation>;
+    onUpdateOrganizationCache: MutationUpdaterFn<UpdateOrganizationMutation>;
+}
+
+function OrganizationForm(props:OrganizationFormProps) {
+    const {
+        id,
+        onAddOrganizationCache,
+        onUpdateOrganizationCache,
+        onHideAddOrganizationModal,
+    } = props;
+
+    const {
+        value,
+        error,
+        onValueChange,
+        validate,
+        onErrorSet,
+        onValueSet,
+    } = useForm(defaultFormValues, schema);
+
+    const {
+        loading: organizationDataLoading,
+        error: organizationDataError,
+    } = useQuery<OrganizationQuery>(
+        ORGANIZATION,
+        {
+            skip: !id,
+            variables: id ? { id } : undefined,
+            onCompleted: (response) => {
+                const { organization } = response;
+
+                if (!organization) {
+                    return;
+                }
+                onValueSet(removeNull({
+                    ...organization,
+                    organizationKind: organization.organizationKind?.id,
+                }));
+            },
+        },
+    );
+
+    const {
+        data: organizationKinds,
+        loading: organizationKindsLoading,
+        error: organizationKindsLoadingError,
+    } = useQuery<OrganizationKindListQuery>(GET_ORGANIZATION_KIND_LIST);
+
+    const organizationKindList = organizationKinds?.organizationKindList?.results;
+
+    const [
+        createOrganization,
+        { loading: createLoading },
+    ] = useMutation<CreateOrganizationMutation, CreateOrganizationMutationVariables>(
+        CREATE_ORGANIZATION_KIND,
+        {
+            update: onAddOrganizationCache,
+            onCompleted: (response) => {
+                const { createOrganization: createOrganizationRes } = response;
+                if (!createOrganizationRes) {
+                    return;
+                }
+                const { errors, result } = createOrganizationRes;
+                if (errors) {
+                    const formError = transformToFormError(removeNull(errors));
+                    onErrorSet(formError);
+                }
+                if (result) {
+                    onHideAddOrganizationModal();
+                }
+            },
+            onError: (errors) => {
+                onErrorSet({
+                    $internal: errors.message,
+                });
+            },
+        },
+    );
+
+    const [
+        updateOrganization,
+        { loading: updateLoading },
+    ] = useMutation<UpdateOrganizationMutation, UpdateOrganizationMutationVariables>(
+        UPDATE_ORGANIZATION_KIND,
+        {
+            update: onUpdateOrganizationCache,
+            onCompleted: (response) => {
+                const { updateOrganization: updateOrganizationRes } = response;
+                if (!updateOrganizationRes) {
+                    return;
+                }
+                const { errors, result } = updateOrganizationRes;
+                if (errors) {
+                    const formError = transformToFormError(removeNull(errors));
+                    onErrorSet(formError);
+                }
+                if (result) {
+                    onHideAddOrganizationModal();
+                }
+            },
+            onError: (errors) => {
+                onErrorSet({
+                    $internal: errors.message,
+                });
+            },
+        },
+    );
+
+    const loading = organizationKindsLoading
+        || createLoading || organizationDataLoading || updateLoading;
+    const errored = !!organizationKindsLoadingError || !!organizationDataError;
+
+    const disabled = loading || errored;
+
+    const handleSubmit = React.useCallback(
+        (finalValues: PartialForm<FormType>) => {
+            if (finalValues.id) {
+                updateOrganization({
+                    variables: {
+                        organization: finalValues as WithId<OrganizationFormFields>,
+                    },
+                });
+            } else {
+                createOrganization({
+                    variables: {
+                        organization: finalValues as OrganizationFormFields,
+                    },
+                });
+            }
+        }, [createOrganization, updateOrganization],
+    );
+
+    return (
+        <form
+            className={styles.form}
+            onSubmit={createSubmitHandler(validate, onErrorSet, handleSubmit)}
+        >
+            {error?.$internal && (
+                <p>
+                    {error?.$internal}
+                </p>
+            )}
+            <div className={styles.twoColumnRow}>
+                <TextInput
+                    label="Name *"
+                    value={value.name}
+                    onChange={onValueChange}
+                    name="name"
+                    error={error?.fields?.name}
+                    disabled={disabled}
+                />
+                <TextInput
+                    label="Short Name *"
+                    value={value.shortName}
+                    onChange={onValueChange}
+                    name="shortName"
+                    error={error?.fields?.shortName}
+                    disabled={disabled}
+                />
+            </div>
+            <div className={styles.row}>
+                <SelectInput
+                    label="Organization Type"
+                    name="organizationKind"
+                    options={organizationKindList}
+                    value={value.organizationKind}
+                    keySelector={basicEntityKeySelector}
+                    labelSelector={basicEntityLabelSelector}
+                    onChange={onValueChange}
+                    error={error?.fields?.organizationKind}
+                    disabled={disabled}
+                />
+            </div>
+            <div className={styles.row}>
+                <TextArea
+                    label="Methodology *"
+                    onChange={onValueChange}
+                    value={value.methodology}
+                    name="methodology"
+                    error={error?.fields?.methodology}
+                    disabled={disabled}
+                />
+            </div>
+            <div className={styles.row}>
+                <TextArea
+                    label="Breakdown"
+                    onChange={onValueChange}
+                    value={value.breakdown}
+                    name="breakdown"
+                    error={error?.fields?.breakdown}
+                    disabled={disabled}
+                />
+            </div>
+            <div className={styles.formButtons}>
+                <Button
+                    type="submit"
+                    name={undefined}
+                    disabled={disabled}
+                    className={styles.button}
+                    variant="primary"
+                >
+                    Submit
+                </Button>
+                <Button
+                    name={undefined}
+                    onClick={onHideAddOrganizationModal}
+                    className={styles.button}
+                    disabled={disabled}
+                >
+                    Cancel
+                </Button>
+            </div>
+        </form>
+    );
+}
+
+export default OrganizationForm;
---
+++ src/components/Organization/index.tsx
@@ -0,0 +1,321 @@
+import React, { useCallback, useState, useMemo } from 'react';
+import { gql, useQuery, MutationUpdaterFn } from '@apollo/client';
+import produce from 'immer';
+import { _cs } from '@togglecorp/fujs';
+import { IoIosSearch } from 'react-icons/io';
+import {
+    TextInput,
+    Table,
+    TableColumn,
+    createColumn,
+    TableHeaderCell,
+    TableHeaderCellProps,
+    TableCell,
+    useSortState,
+    TableSortDirection,
+    Pager,
+    Modal,
+    Button,
+} from '@togglecorp/toggle-ui';
+
+import Container from '#components/Container';
+import useModalState from '#hooks/useModalState';
+import { ExtractKeys } from '#types';
+
+import {
+    OrganizationsListQuery,
+    UpdateOrganizationMutation,
+} from '#generated/types';
+
+import DateCell from '#components/tableHelpers/Date';
+
+import Loading from '#components/Loading';
+
+import ActionCell, { ActionProps } from '#components/tableHelpers/Action';
+import styles from './styles.css';
+import OrganizationForm from './OrganizationForm';
+
+// TODO - update nameContains in backend
+const GET_ORGANIZATIONS_LIST = gql`
+query OrganizationsList($ordering: String, $page: Int, $pageSize: Int, $name: String) {
+    organizationList(ordering: $ordering, page: $page, pageSize: $pageSize, name: $name) {
+      results {
+        id
+        name
+        createdAt
+        shortName
+        breakdown
+        methodology
+        organizationKind {
+            id
+            name
+        }
+      }
+      totalCount
+      pageSize
+      page
+    }
+  }
+`;
+
+interface OrganizationProps {
+    className? : string;
+}
+
+const defaultSortState = {
+    name: 'createdAt',
+    direction: TableSortDirection.dsc,
+};
+
+type OrganizationFields = NonNullable<NonNullable<OrganizationsListQuery['organizationList']>['results']>[number];
+
+interface Entity {
+    id: string;
+    name: string | undefined;
+}
+
+const keySelector = (item: OrganizationFields) => item.id;
+
+function Organization(props: OrganizationProps) {
+    const {
+        className,
+    } = props;
+
+    const { sortState, setSortState } = useSortState();
+    const validSortState = sortState || defaultSortState;
+
+    const ordering = validSortState.direction === TableSortDirection.asc
+        ? validSortState.name
+        : `-${validSortState.name}`;
+
+    const [page, setPage] = useState(1);
+    const [search, setSearch] = useState<string | undefined>();
+    const [pageSize, setPageSize] = useState(25);
+
+    const [organizationIdOnEdit, setOrganizationIdOnEdit] = useState<OrganizationFields['id'] | undefined>();
+
+    const [
+        shouldShowAddOrganizationModal,
+        showAddOrganizationModal,
+        hideAddOrganizationModal,
+    ] = useModalState();
+
+    const variables = useMemo(
+        () => ({
+            ordering,
+            page,
+            pageSize,
+            name: search,
+        }),
+        [ordering, page, pageSize, search],
+    );
+
+    const {
+        data: organizations,
+        loading: organizationsLoading,
+        refetch: refetchOrganizationList,
+        // TODO: handle error
+    } = useQuery<OrganizationsListQuery>(GET_ORGANIZATIONS_LIST, { variables });
+
+    // eslint-disable-next-line max-len
+    const handleUpdateOrganizationCache: MutationUpdaterFn<UpdateOrganizationMutation> = useCallback(
+        (cache, data) => {
+            const organization = data?.data?.updateOrganization?.result;
+            if (!organization) {
+                return;
+            }
+
+            const cacheData = cache.readQuery<OrganizationsListQuery>({
+                query: GET_ORGANIZATIONS_LIST,
+                variables,
+            });
+
+            const updatedValue = produce(cacheData, (safeCacheData) => {
+                if (!safeCacheData?.organizationList?.results) {
+                    return;
+                }
+                const { results } = safeCacheData.organizationList;
+                const organizationIndex = results.findIndex((res) => res.id === organization.id);
+                if (organizationIndex !== -1) {
+                    results.splice(organizationIndex, 1);
+                }
+            });
+
+            if (updatedValue === cacheData) {
+                return;
+            }
+
+            cache.writeQuery({
+                query: GET_ORGANIZATIONS_LIST,
+                data: updatedValue,
+            });
+        },
+        [variables],
+    );
+
+    const handleRefetch = useCallback(
+        () => {
+            refetchOrganizationList(variables);
+        },
+        [refetchOrganizationList, variables],
+    );
+
+    const handleHideAddOrganizationModal = useCallback(
+        () => {
+            setOrganizationIdOnEdit(undefined);
+            hideAddOrganizationModal();
+        },
+        [hideAddOrganizationModal, setOrganizationIdOnEdit],
+    );
+
+    const handleSetOrganizationIdOnEdit = useCallback(
+        (organizationId) => {
+            setOrganizationIdOnEdit(organizationId);
+            showAddOrganizationModal();
+        },
+        [setOrganizationIdOnEdit, showAddOrganizationModal],
+    );
+
+    const loading = organizationsLoading;
+
+    const organizationColumns = useMemo(
+        () => {
+            type stringKeys = ExtractKeys<OrganizationFields, string>;
+            type entityKeys = ExtractKeys<OrganizationFields, Entity>;
+
+            // Generic columns
+            const stringColumn = (colName: stringKeys) => ({
+                headerCellRenderer: TableHeaderCell,
+                headerCellRendererParams: {
+                    onSortChange: setSortState,
+                    sortable: false,
+                    sortDirection: colName === validSortState.name
+                        ? validSortState.direction
+                        : undefined,
+                },
+                cellRenderer: TableCell,
+                cellRendererParams: (_: string, datum: OrganizationFields) => ({
+                    value: datum[colName],
+                }),
+            });
+
+            const entityColumn = (colName: entityKeys) => ({
+                headerCellRenderer: TableHeaderCell,
+                headerCellRendererParams: {
+                    onSortChange: setSortState,
+                    sortable: true,
+                    sortDirection: colName === validSortState.name
+                        ? validSortState.direction
+                        : undefined,
+                },
+                cellRenderer: TableCell,
+                cellRendererParams: (_: string, datum: OrganizationFields) => ({
+                    value: datum[colName]?.name,
+                }),
+            });
+
+            const dateColumn = (colName: stringKeys) => ({
+                headerCellRenderer: TableHeaderCell,
+                headerCellRendererParams: {
+                    onSortChange: setSortState,
+                    sortable: true,
+                    sortDirection: colName === validSortState.name
+                        ? validSortState.direction
+                        : undefined,
+                },
+                cellRenderer: DateCell,
+                cellRendererParams: (_: string, datum: OrganizationFields) => ({
+                    value: datum[colName],
+                }),
+            });
+
+            // eslint-disable-next-line max-len
+            const actionColumn: TableColumn<OrganizationFields, string, ActionProps, TableHeaderCellProps> = {
+                id: 'action',
+                title: '',
+                headerCellRenderer: TableHeaderCell,
+                headerCellRendererParams: {
+                    sortable: false,
+                },
+                cellRenderer: ActionCell,
+                cellRendererParams: (_, datum) => ({
+                    id: datum.id,
+                    onEdit: handleSetOrganizationIdOnEdit,
+                }),
+            };
+
+            return [
+                createColumn(dateColumn, 'createdAt', 'Date Created'),
+                createColumn(stringColumn, 'name', 'Name'),
+                createColumn(stringColumn, 'shortName', 'Short Name'),
+                createColumn(stringColumn, 'methodology', 'Methodology'),
+                createColumn(stringColumn, 'breakdown', 'Breakdown'),
+                createColumn(entityColumn, 'organizationKind', 'Organization Kind'),
+                actionColumn,
+            ];
+        },
+        [
+            setSortState,
+            validSortState,
+            handleSetOrganizationIdOnEdit,
+        ],
+    );
+
+    return (
+        <Container
+            heading="Organizations"
+            className={_cs(className, styles.container)}
+            headerActions={(
+                <>
+                    <TextInput
+                        icons={<IoIosSearch />}
+                        name="search"
+                        value={search}
+                        placeholder="Search"
+                        onChange={setSearch}
+                    />
+                    <Button
+                        name={undefined}
+                        onClick={showAddOrganizationModal}
+                        label="Add New Organization"
+                        disabled={loading}
+                    >
+                        Add New Organization
+                    </Button>
+                </>
+            )}
+            footerContent={(
+                <Pager
+                    activePage={page}
+                    itemsCount={organizations?.organizationList?.totalCount ?? 0}
+                    maxItemsPerPage={pageSize}
+                    onActivePageChange={setPage}
+                    onItemsPerPageChange={setPageSize}
+                />
+            )}
+        >
+            {loading && <Loading />}
+            <Table
+                className={styles.table}
+                data={organizations?.organizationList?.results}
+                keySelector={keySelector}
+                columns={organizationColumns}
+            />
+            {shouldShowAddOrganizationModal && (
+                <Modal
+                    onClose={handleHideAddOrganizationModal}
+                    heading={organizationIdOnEdit ? 'Edit Organization' : 'Add New Organization'}
+                >
+                    <OrganizationForm
+                        id={organizationIdOnEdit}
+                        onAddOrganizationCache={handleRefetch}
+                        onUpdateOrganizationCache={handleUpdateOrganizationCache}
+                        onHideAddOrganizationModal={handleHideAddOrganizationModal}
+                    />
+                </Modal>
+            )}
+        </Container>
+    );
+}
+
+export default Organization;
