b18189b3b858b515ce52c35a7bc7d276a2f35818
barshathakuri <barshathakuri708@gmail.com>
2021-03-01 17:13:19 +0545

    Added upload profile picture

--- src/components/UserProfileUpdateForm/index.tsx
+++ src/components/UserProfileUpdateForm/index.tsx
@@ -23,8 +23,11 @@ import {
     UserQuery,
     UpdateUserMutation,
     UpdateUserMutationVariables,
+    CreateAttachmentMutation,
+    CreateAttachmentMutationVariables,
 } from '#generated/types';
 
+import FileUploader from '#components/FileUploader';
 import NonFieldError from '#components/NonFieldError';
 import NotificationContext from '#components/NotificationContext';
 import Loading from '#components/Loading';
@@ -32,14 +35,11 @@ import Loading from '#components/Loading';
 import styles from './styles.css';
 
 const GET_USER = gql`
-    query UserProfile($id: ID!) {
+    query PullUserProfile($id: ID!) {
         user(id: $id) {
             id
-            fullName
-            username
             firstName
             lastName
-            role
         }
     }
 `;
@@ -49,20 +49,30 @@ const UPDATE_USER = gql`
         updateUser(data: $data) {
             result {
                 id
-                fullName
-                username
                 firstName
                 lastName
-                role
             }
             errors
         }
     }
 `;
 
+const UPLOAD_PROFILE_PICTURE = gql`
+    mutation UploadProfilePicture($attachment: Upload!) {
+        createAttachment(data: {attachment: $attachment, attachmentFor: "1"}) {
+            errors
+            ok
+            result {
+                attachment
+                id
+            }
+        }
+    }
+`;
+
 // eslint-disable-next-line @typescript-eslint/ban-types
 type UserFormFields = UpdateUserMutationVariables['data'];
-type FormType = PurgeNull<PartialForm<Omit<UserFormFields, 'role'> & { role: string }>>;
+type FormType = PurgeNull<PartialForm<UserFormFields>>;
 
 type FormSchema = ObjectSchema<FormType>
 type FormSchemaFields = ReturnType<FormSchema['fields']>;
@@ -164,7 +174,46 @@ function UserForm(props:UserFormProps) {
         }, [updateUser],
     );
 
-    const loading = userLoading || updateLoading;
+    const [
+        uploadProfielPicture,
+        { loading: imageLoading },
+    ] = useMutation<CreateAttachmentMutation, CreateAttachmentMutationVariables>(
+        UPLOAD_PROFILE_PICTURE,
+        {
+            onCompleted: (response) => {
+                const { createAttachment: createAttachmentRes } = response;
+                if (!createAttachmentRes) {
+                    return;
+                }
+                const { errors, result } = createAttachmentRes;
+                if (errors) {
+                    console.log('ERROR: Field to upload', errors);
+                    notify({ children: 'Failed to create attachment' });
+                }
+                if (result) {
+                    console.log('SUCCESS', result);
+                }
+            },
+            onError: (err) => {
+                console.log('error', err);
+                notify({ children: err.message });
+            },
+        },
+    );
+
+    const handleAttachmentProcess = useCallback(
+        (files: File[]) => {
+            uploadProfielPicture({
+                variables: { attachment: files[0] },
+                context: {
+                    hasUpload: true, // activate Upload link
+                },
+            });
+        },
+        [uploadProfielPicture],
+    );
+
+    const loading = userLoading || updateLoading || imageLoading;
     const errored = !!userError;
     const disabled = loading || errored;
 
@@ -194,6 +243,16 @@ function UserForm(props:UserFormProps) {
                     error={error?.fields?.lastName}
                     disabled={disabled}
                 />
+            </div>
+            <div className={styles.row}>
+                <FileUploader
+                    onChange={handleAttachmentProcess}
+                    variant="primary"
+                    accept="image/png, image/jpeg"
+                    disabled={disabled}
+                >
+                    Upload Profile Picture
+                </FileUploader>
             </div>
             <div className={styles.formButtons}>
                 <Button
