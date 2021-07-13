4a1afeb31f05aca47419ffedd174a124dc484204
Priyesh777 <priyeshgautam1010@gmail.com>
2021-07-13 09:00:47 +0545

    Fix the filter for communication table

---
+++ src/components/tables/CommunicationAndPartners/CommunicationTable/CommunicationFilter/index.tsx
@@ -0,0 +1,118 @@
+import React, { useCallback } from 'react';
+import { TextInput, Button } from '@togglecorp/toggle-ui';
+import { _cs } from '@togglecorp/fujs';
+import {
+    ObjectSchema,
+    useForm,
+    createSubmitHandler,
+} from '@togglecorp/toggle-form';
+
+import {
+    IoIosSearch,
+} from 'react-icons/io';
+import NonFieldError from '#components/NonFieldError';
+
+import { PartialForm, PurgeNull } from '#types';
+import { CommunicationListQueryVariables } from '#generated/types';
+import styles from './styles.css';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type CommunicationFilterFields = Omit<CommunicationListQueryVariables, 'ordering' | 'page' | 'pageSize'>;
+type FormType = PurgeNull<PartialForm<CommunicationFilterFields>>;
+
+type FormSchema = ObjectSchema<FormType>
+type FormSchemaFields = ReturnType<FormSchema['fields']>;
+
+const schema: FormSchema = {
+    fields: (): FormSchemaFields => ({
+        subject: [],
+    }),
+};
+
+const defaultFormValues: PartialForm<FormType> = {
+    subject: undefined,
+};
+
+interface ContactsFilterProps {
+    className?: string;
+    onFilterChange: (value: PurgeNull<CommunicationListQueryVariables>) => void;
+}
+
+function ContactsFilter(props: ContactsFilterProps) {
+    const {
+        className,
+        onFilterChange,
+    } = props;
+
+    const {
+        pristine,
+        value,
+        error,
+        onValueChange,
+        validate,
+        onErrorSet,
+        onValueSet,
+    } = useForm(defaultFormValues, schema);
+
+    const onResetFilters = useCallback(
+        () => {
+            onValueSet(defaultFormValues);
+            onFilterChange(defaultFormValues);
+        },
+        [onValueSet, onFilterChange],
+    );
+
+    const handleSubmit = React.useCallback((finalValues: FormType) => {
+        onValueSet(finalValues);
+        onFilterChange(finalValues);
+    }, [onValueSet, onFilterChange]);
+
+    const filterChanged = defaultFormValues !== value;
+
+    return (
+        <form
+            className={_cs(className, styles.queryForm)}
+            onSubmit={createSubmitHandler(validate, onErrorSet, handleSubmit)}
+        >
+            <NonFieldError>
+                {error?.$internal}
+            </NonFieldError>
+            <div className={styles.contentContainer}>
+                <div className={styles.inputContainer}>
+                    <TextInput
+                        className={styles.input}
+                        icons={<IoIosSearch />}
+                        label="Subject"
+                        name="subject"
+                        value={value.subject}
+                        onChange={onValueChange}
+                        error={error?.fields?.subject}
+                    />
+                </div>
+                <div className={styles.formButtons}>
+                    <Button
+                        name={undefined}
+                        onClick={onResetFilters}
+                        title="Reset"
+                        disabled={!filterChanged}
+                        className={styles.button}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        name={undefined}
+                        type="submit"
+                        title="Apply"
+                        disabled={pristine}
+                        className={styles.button}
+                        variant="primary"
+                    >
+                        Apply
+                    </Button>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default ContactsFilter;
