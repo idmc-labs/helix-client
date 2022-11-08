c0af1f09912cdb7c5f14d85f29c57a82689407c6
puranban <puranban77@gmail.com>
2022-11-08 14:48:03 +0545

    Add figure list in event review

--- src/views/EventReview/index.tsx
+++ src/views/EventReview/index.tsx
@@ -1,16 +1,25 @@
-import React, { useMemo } from 'react';
+import React, {
+    useMemo,
+    useState,
+} from 'react';
 import { useParams } from 'react-router-dom';
-import { _cs } from '@togglecorp/fujs';
+import { isDefined, _cs } from '@togglecorp/fujs';
 import { useQuery } from '@apollo/client';
+import { removeNull } from '@togglecorp/toggle-form';
 
 import { FIGURE_LIST } from '#views/Entry/EntryForm/queries';
 import EventForm from '#components/forms/EventForm';
 import PageHeader from '#components/PageHeader';
+import { EventListOption } from '#components/selections/EventListSelectInput';
 import {
     FigureListQuery,
     FigureListQueryVariables,
 } from '#generated/types';
-import FigureAndPreview from './FigureandPreview';
+import {
+    Attachment,
+    SourcePreview,
+} from '#views/Entry/EntryForm/types';
+import FigureAndPreview from './FigureAndPreview';
 
 import styles from './styles.css';
 
@@ -21,8 +30,23 @@ interface Props {
 function EventReview(props: Props) {
     const { className } = props;
     const { eventId } = useParams<{ eventId: string }>();
+    const [selectedFigure, setSelectedFigure] = useState<string | undefined>();
+    const [attachment, setAttachment] = useState<Attachment | undefined>(undefined);
+    const [preview, setPreview] = useState<SourcePreview | undefined>(undefined);
+    const [
+        events,
+        setEvents,
+    ] = useState<EventListOption[] | null | undefined>([]);
 
-    // TODO:
+    const figureId = useMemo(
+        () => {
+            const params = new URLSearchParams(document.location.search);
+            return params.get('id');
+        },
+        [],
+    );
+
+    // FIXME: type error
     const variables = useMemo(
         (): FigureListQueryVariables | undefined => (
             eventId ? { event: eventId } : undefined
@@ -31,11 +55,25 @@ function EventReview(props: Props) {
     );
 
     const {
+        loading: getFiguresLoading,
         data: figureListResponse,
-        loading: loadingFigureResponse,
     } = useQuery<FigureListQuery, FigureListQueryVariables>(FIGURE_LIST, {
         skip: !eventId,
         variables,
+        onCompleted: (response) => {
+            const { figureList } = removeNull(response);
+            setEvents(figureList?.results?.map((item) => item.event).filter(isDefined));
+
+            const mainFigure = figureList?.results?.find((element) => element.id === figureId);
+            setSelectedFigure(mainFigure?.uuid);
+
+            if (mainFigure?.entry.preview) {
+                setPreview(mainFigure.entry.preview);
+            }
+            if (mainFigure?.entry.document) {
+                setAttachment(mainFigure.entry.document);
+            }
+        },
     });
 
     return (
@@ -49,7 +87,16 @@ function EventReview(props: Props) {
                     id={eventId}
                 />
             </div>
-            <FigureAndPreview />
+            <FigureAndPreview
+                loading={getFiguresLoading}
+                figures={figureListResponse?.figureList?.results}
+                setSelectedFigure={setSelectedFigure}
+                selectedFigure={selectedFigure}
+                events={events}
+                setEvents={setEvents}
+                attachment={attachment}
+                preview={preview}
+            />
         </div>
     );
 }
