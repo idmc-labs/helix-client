5a0fb808de40e5c114ddd57e3f1b97819a164942
Priyesh777 <priyeshgautam1010@gmail.com>
2022-11-17 16:34:56 +0545

    Add dummy daya for notifications list

---
+++ src/utils/dummyData.ts
@@ -0,0 +1,51 @@
+export interface FakeTypeData {
+    id: string;
+    section: string;
+    description: string;
+    dateCreated: string;
+}
+
+export const notificationsData: FakeTypeData[] = [
+    {
+        id: '001',
+        section: 'Event',
+        description: 'Some description of event 1',
+        dateCreated: '2010-10-11',
+    },
+    {
+        id: '002',
+        section: 'QA',
+        description: 'Some description of QA event',
+        dateCreated: '2010-10-11',
+    },
+    {
+        id: '003',
+        section: 'Report',
+        description: 'Some description of report 1',
+        dateCreated: '2010-10-11',
+    },
+    {
+        id: '004',
+        section: 'Report',
+        description: 'Some description of report 2',
+        dateCreated: '2010-10-11',
+    },
+    {
+        id: '001',
+        section: 'Event',
+        description: 'Some description of event 1',
+        dateCreated: '2010-10-11',
+    },
+    {
+        id: '001',
+        section: 'Event',
+        description: 'Some description of event 1',
+        dateCreated: '2010-10-11',
+    },
+    {
+        id: '001',
+        section: 'Event',
+        description: 'Some description of event 1',
+        dateCreated: '2010-10-11',
+    },
+];
