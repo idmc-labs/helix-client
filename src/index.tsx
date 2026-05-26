5dc83e820ab2cbcb6fc80b5f5a28a2e3c93f34a1
subinasr <ssr.subina@gmail.com>
2026-05-26 15:37:34 +0545

    add(packages): google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga4';
 
 import Root from './Root';
 
 console.info('React version', React.version);
+
+const TRACKING_ID = import.meta.env.REACT_APP_GOOGLE_ANALYTICS_ID;
+
+if (TRACKING_ID) {
+    ReactGA.initialize(TRACKING_ID);
+}
 
 const rootElement = document.getElementById('helix-client-root');
 if (rootElement) {
