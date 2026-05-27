38ce8dfda95fd2e7d9d30d6bec253bb5ac306d1d
subinasr <ssr.subina@gmail.com>
2026-05-27 15:05:30 +0545

    add(google-analytics): content groups

--- src/Root/index.tsx
+++ src/Root/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
-import { Router, useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga4';
+import React from 'react';
+import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import {
     init,
@@ -56,15 +55,6 @@ if (sentryDsn) {
 if (mapboxToken) {
     mapboxgl.accessToken = mapboxToken;
 }
-const AnalyticsTracker = () => {
-    const location = useLocation();
-
-    useEffect(() => {
-        ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
-    }, [location]);
-
-    return null;
-};
 
 // TODO: upload sourcemaps
 // TODO: track performance monitoring
@@ -79,7 +69,6 @@ function Root(props: Props) {
             showDialog
         >
             <Router history={history}>
-                <AnalyticsTracker />
                 <App {...props} />
             </Router>
         </ErrorBoundary>
