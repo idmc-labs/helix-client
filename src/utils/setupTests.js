b35beaa094026a0e9c88d61b38b2ea7e8d82c0e3
tnagorra <weathermist@gmail.com>
2026-08-04 08:45:34 +0545

    chore(test): satisfy eslint no-var-requires in setupTests

--- src/utils/setupTests.js
+++ src/utils/setupTests.js
@@ -4,9 +4,9 @@
 // (modern cheerio uses `node:` imports that Jest 26 cannot resolve). Component
 // tests that actually need enzyme should repair this bootstrap.
 try {
-    // eslint-disable-next-line global-require
+    // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
     const Enzyme = require('enzyme');
-    // eslint-disable-next-line global-require
+    // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
     const Adapter = require('enzyme-adapter-react-16');
     // eslint-disable-next-line global-require, no-unused-vars
     require('./tempPolyfills');
